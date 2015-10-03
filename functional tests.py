bf30897c2faadb21107221cef3275fdeec38cd0b
lex-quarkie <lex.quarkie@gmail.com>
2015-10-03 12:29:58 +0300

    ok

--- functional tests.py
+++ functional tests.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase): 
 
-assert 'Django' in browser.title
+
+# setUp and tearDown are special methods which get run before and after each test. 
+# I’m using them to start and stop our browser—note that they’re a bit like a try/except,
+# in that tearDown will run even if there’s an error during the test itself.[4] 
+# No more Firefox windows left lying around!
+    def setUp(self):
+        self.Browser = webdriver.Firefox()
+
+    def TearDown(self):
+        self.Browser.quit()
+
+    def test_start_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.Browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.Browser.title)  #We use self.assertIn instead of just assert to make our test assertions. unittest provides lots of helper functions like this to make test assertions, like assertEqual, assertTrue, assertFalse, and so on. 
+        self.fail('Finish the test!')  # We use self.assertIn instead of just assert to make our test assertions. unittest provides lots of helper functions like this to make test assertions, like assertEqual, assertTrue, assertFalse, and so on. 
+
+
+if __name__ == '__main__':  #  we have the if __name__ == '__main__' clause (if you’ve not seen it before, that’s how a Python script checks if it’s been executed from the command line, rather than just imported by another script). We call  unittest.main(), 
+    unittest.main(warnings='ignore')
