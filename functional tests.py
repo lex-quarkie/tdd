1419f5988372dbeef12697da91fedd011137f160
lex-quarkie <lex.quarkie@gmail.com>
2015-10-04 16:00:29 +0300

    Front Page HTML now generated from a template

--- functional tests.py
+++ functional tests.py
@@ -1,28 +1,53 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 import unittest
 
-class NewVisitorTest(unittest.TestCase): 
+class NewVisitorTest(unittest.TestCase):
 
-
-# setUp and tearDown are special methods which get run before and after each test. 
-# in that tearDown will run even if there’s an error during the test itself.[4] 
-# I’m using them to start and stop our browser—note that they’re a bit like a try/except,
-# No more Firefox windows left lying around!
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3) 
+        self.browser.implicitly_wait(.6)
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_start_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)  #We use self.assertIn instead of just assert to make our test assertions. unittest provides lots of helper functions like this to make test assertions, like assertEqual, assertTrue, assertFalse, and so on. 
-        self.fail('Finish the test!')  # We use self.assertIn instead of just assert to make our test assertions. unittest provides lots of helper functions like this to make test assertions, like assertEqual, assertTrue, assertFalse, and so on. 
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+        "New to-do item did not appear in table"
+        )
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':  #  we have the if __name__ == '__main__' clause (if you’ve not seen it before, that’s how a Python script checks if it’s been executed from the command line, rather than just imported by another script). We call  unittest.main()
