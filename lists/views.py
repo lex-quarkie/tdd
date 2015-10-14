b3af9ce26b6ab9622e75eb1b3cb37199d454d344
lex-quarkie <lex.quarkie@gmail.com>
2015-10-14 17:29:01 +0300

    /the-only-list-URL pass

--- lists/views.py
+++ lists/views.py
@@ -4,8 +4,8 @@ from lists.models import Item
 
 def home_page(request):
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'] ) # neat shortcut to creating Item, without calling .save
-        return redirect('/')
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/lists/the-only-list-in-the-world/')
 
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
