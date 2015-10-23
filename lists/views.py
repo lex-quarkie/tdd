a4dd839d48db5a22599866eb0fb6f7bebccc21dd
lex-quarkie <lex.quarkie@gmail.com>
2015-10-23 09:28:51 +0300

    pre: Adjusting_our_models

--- lists/views.py
+++ lists/views.py
@@ -14,5 +14,6 @@ def view_list(request):
 	return render(request, 'list.html', {'items': items})
 
 def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
     return redirect('/lists/the-only-list-in-the-world/')
 
